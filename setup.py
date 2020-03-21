bce6f193ab777c7c1bbe45e61fa31a027b9bd97e
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-03-21 21:57:54 +0100

    Update the Python versions

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',

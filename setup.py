8178199bc63080309af54a746f92aa91506a9aa8
Vladimir.Roschin <vladimir.roschin@rt.ru>
2020-04-09 23:11:51 +0300

    minor refactor setup.py based on pylint warnings

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-"""
-pip-review lets you smoothly manage all available PyPI updates.
-"""
+""" pip-review lets you smoothly manage all available PyPI updates. """
+
 from setuptools import setup
 
 
@@ -22,7 +21,7 @@ setup(
             'pip-review = pip_review.__main__:main',
         ],
     },
-    #include_package_data=True,
+    # include_package_data=True,
     zip_safe=False,
     platforms='any',
     install_requires=[
@@ -32,24 +31,24 @@ setup(
     python_requires='>=2.7, !=3.0, !=3.1, !=3.2',
     classifiers=[
         # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        #'Development Status :: 1 - Planning',
-        #'Development Status :: 2 - Pre-Alpha',
-        #'Development Status :: 3 - Alpha',
-        #'Development Status :: 4 - Beta',
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
         'Development Status :: 5 - Production/Stable',
-        #'Development Status :: 6 - Mature',
-        #'Development Status :: 7 - Inactive',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.3',
-        #'Programming Language :: Python :: 2.4',
-        #'Programming Language :: Python :: 2.5',
-        #'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.3',
+        # 'Programming Language :: Python :: 2.4',
+        # 'Programming Language :: Python :: 2.5',
+        # 'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.0',
-        #'Programming Language :: Python :: 3.1',
-        #'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+        # 'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
